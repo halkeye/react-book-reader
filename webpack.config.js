2999c7004fc53dc49238e77bd85b7e8727b9c102
Gavin Mogan <gavin@gavinmogan.com>
2015-05-31 19:15:28 -0700

    Eslint that webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,48 @@
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require("path");
+'use strict';
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 
-const sassLoaders = [
-  "css-loader",
-  "autoprefixer-loader?browsers=last 2 version",
-  "sass-loader?indentedSyntax=sass&includePaths[]=" + path.resolve(__dirname, "./src")
+var sassLoaders = [
+  'css-loader',
+  'autoprefixer-loader?browsers=last 2 version',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './src')
 ];
 
-const scssLoaders = [
-  "css-loader",
-  "autoprefixer-loader?browsers=last 2 version",
-  "sass-loader?includePaths[]=" + path.resolve(__dirname, "./src")
+var scssLoaders = [
+  'css-loader',
+  'autoprefixer-loader?browsers=last 2 version',
+  'sass-loader?includePaths[]=' + path.resolve(__dirname, './src')
 ];
 
-const config = {
-  entry: ["./src/js/index"],
+var config = {
+  entry: ['./src/js/index'],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ["babel-loader"]
+        loaders: ['babel-loader']
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract("style-loader", scssLoaders.join("!"))
+        loader: ExtractTextPlugin.extract('style-loader', scssLoaders.join('!'))
       },
       {
         test: /\.sass$/,
-        loader: ExtractTextPlugin.extract("style-loader", sassLoaders.join("!"))
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
       },
-      { test: /\.png$/, loader: "file-loader" }
+      { test: /\.png$/, loader: 'file-loader' }
     ]
   },
   output: {
-    filename: "js/[name]-[hash].js",
-    path: path.join(__dirname, "./dist"),
-    publicPath: "/"
+    filename: 'js/[name]-[hash].js',
+    path: path.join(__dirname, './dist'),
+    publicPath: '/'
   },
   plugins: [
-    new ExtractTextPlugin("styles/[name]-[hash].css"),
+    new ExtractTextPlugin('styles/[name]-[hash].css'),
     new HtmlWebpackPlugin({
       inject: true,
       title: 'Books Books Books',
@@ -51,8 +52,8 @@ const config = {
     new webpack.DefinePlugin({GA_TRACKING_CODE: JSON.stringify('UA-89920-17')})
   ],
   resolve: {
-    extensions: ["", ".jsx", ".js", ".sass", ".scss"],
-    modulesDirectories: ["src", "node_modules"]
+    extensions: ['', '.jsx', '.js', '.sass', '.scss'],
+    modulesDirectories: ['src', 'node_modules']
   }
 };
 
