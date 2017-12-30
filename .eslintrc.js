de4ae8be0b59ef2b15141831bdff2fd75e393066
Gavin Mogan <git@gavinmogan.com>
2017-12-30 10:15:45 -0800

    eslint that jest too

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,31 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": [
-      "plugin:react/recommended",
-      "plugin:import/errors",
-      "plugin:import/warnings",
-      "standard"
-    ],
-    "plugins": [
-        "standard",
-        "promise",
-        "import",
-      // "jsx-a11y",
-        "react"
-    ],
-    "rules": {
-      "semi": [2, "always"],
-      "no-var": "error"
-    },
-    "env": {
-      "browser": true,
-      "es6": true
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        }
+  parser: 'babel-eslint',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jest/recommended',
+    'standard'
+  ],
+  plugins: [
+    'standard',
+    'promise',
+    'import',
+    'jest',
+    // "jsx-a11y",
+    'react'
+  ],
+  rules: {
+    semi: [2, 'always'],
+    'no-var': 'error'
+  },
+  env: {
+    browser: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     }
+  }
 };
