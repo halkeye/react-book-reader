f971e93f5edada391a773e7a9645575edec19a27
Gavin Mogan <git@gavinmogan.com>
2017-12-31 18:19:49 -0800

    Fix

--- src/js/components/ProgressBar.jsx
+++ src/js/components/ProgressBar.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Line, Circle } from 'rc-progress';
+import React from "react";
+import PropTypes from "prop-types";
 
-export default function ProgressBar ({ total, loaded }) {
+export default function ProgressBar({ total, loaded }) {
   if (total === null || loaded === null) {
     return null;
   }
@@ -12,12 +11,14 @@ export default function ProgressBar ({ total, loaded }) {
     percent = loaded / total * 100;
   }
 
+  let style = {
+    width: Math.max(0, Math.min(percent, 100)) + "%",
+    transition: "width 200ms"
+  };
+
   return (
-    <div>
-      <h1>Loading {percent}%</h1>
-      <div style={{ margin: 10, width: 600 }}>
-        <Circle percent={percent} strokeWidth="6" />;
-      </div>
+    <div className="progressbar-container">
+      <div className="progressbar-progress" style={style} />
     </div>
   );
 }
