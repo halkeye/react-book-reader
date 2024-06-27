6991c2e9cf6d4d4449978b5989951f6e9ef490c6
Gavin Mogan <git@gavinmogan.com>
2024-06-26 20:51:07 -0700

    more eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,36 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:promise/recommended',
     'plugin:unicorn/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'promise', 'unicorn'],
+  plugins: [
+    'react-refresh',
+    'promise',
+    'unicorn',
+    'eslint-comments',
+    'import',
+    'no-only-tests',
+    'jsx-a11y',
+  ],
   rules: {
+    'no-only-tests/no-only-tests': 'error',
     'object-shorthand': ['error', 'methods'],
     '@typescript-eslint/array-type': ['error', { default: 'generic' }],
     'prefer-template': 'error',
